c1ca4aee65abd684a66b305fc54451714177cdc6
zubijvd <vijayd@steepgraph.com>
2025-01-23 15:19:02 +0530

    java02

--- src/Ajavabasics/DataTypesConcept.java
+++ src/Ajavabasics/DataTypesConcept.java
@@ -5,21 +5,22 @@ public class DataTypesConcept {
 	public static void main(String[] args) {
 		//:-2024 Java sessions: Java_02_DataTypes_Range_Size_PrimitiveDataTypes.mp4
 
-		
+		//****************************************************************************************************
 		//A: Data Types
 		//**1.primitive data types 
 		
 		//Numeric Type:
 		    //integral value 
 		              //integer : byte, short, int and long
-		              //floating: float and double c1
+		              //floating: float and double 
 		   //character: char : $ 1 a
 	   //boolean type : true , false
+		
 		
 	   //**2.non primitive data types: class , interface,arrays AND object
 		
 		
-		
+		//****************************************************************************************************
 		//1. byte: staring with small letter is called "Keyword" in java.
 		byte b = 10;
 		//byte b = 20;  Can create duplicate variable "b" in java
@@ -35,7 +36,7 @@ public class DataTypesConcept {
 		System.out.println(c1);
 		
 		
-		
+		//****************************************************************************************************
 		//2.shorts
 		//size: 2 byte == 16 bits
 		//range: -32768 to 32767 (2^15 to 2^15-1)
@@ -44,6 +45,8 @@ public class DataTypesConcept {
 	    System.out.println(sh);
 	    System.out.println(sh1);
 	    
+	    
+	    //****************************************************************************************************
 	    //3.Integer:
 	    //size:4 bytes = 4*8 = 32 bits
 	    //range: -2147483648 to 2147483647 (2^31 to 2^31-1)
@@ -54,14 +57,65 @@ public class DataTypesConcept {
 	    System.out.println(j);
 	    
 	    
+	    //****************************************************************************************************
 	    //4.long
 	    //size: 8*8 =64bits
 	    //range: (2^63 to 2^63-1)
+	    long num = 18523694744555L; //for large value put L in value:
+	    System.out.println(num);
+	    System.out.println(num); // We can use for population, earth distance,sun
 	    
-	    long l = 18523694744555L;
-	    System.out.println(l);
+	    //mobile, ssn ,adhar,card number, account ,this will store in string.
 	    
-	    System.out.println(l);
+	    
+	    //****************************************************************************************************
+	    //5.float : it will store decimal value:
+	    //size: 4 bytes = 4*8 = 32bits
+	    //range: after dot . it can take upto 6-7 digits and before dot 39Digits
+	    float f = 12.33f; //Write F in float value:
+	    float f1 = (float)12.33;
+	    System.out.println(f);
+	    System.out.println(f1);
+	    float f2 =100;
+	    System.out.println(f2);
+	    
+	    
+	    //****************************************************************************************************
+	    //6.double:
+	    //size: 8 bytes = 64 bits
+	    //range: it can take upto 15 decimal digits before dots and after dot 309 digits
+	    double de  =12.33;
+	    System.out.println(de);
+	    
+	    double df = 125; // can store pure numeric value but it will print 125.0
+	    System.out.println(df);
+	    
+	    
+	    
+	    //****************************************************************************************************
+	    //7.char: it can only take 1 digit value:unicode values +ASCII values
+	    //size: 2 bytes = 16 bits
+	    char ch ='a';
+	    System.out.println(ch);
+	    char ch1 = '%';
+	    System.out.println(ch1);
+	    
+	    char gender = 'M';
+	    System.out.println(gender);
+	    
+	    char ch2 = 123; //it will store cz it come from numeric family.
+	    System.out.println(ch2);
+	    
+	    
+	    //****************************************************************************************************
+	    //8.boolean: 
+	    //size: ~1  bit
+	    //range: true or false:
+	    
+	    boolean bl  = true;
+	    boolean b2  = false;
+	    System.out.println(bl);
+	    System.out.println(b2);
 	    
 		
 		
---
+++ src/Ajavabasics/StringConcatenation.java
@@ -0,0 +1,31 @@
+package Ajavabasics;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		//Java_03_StringDataType_StringConcatenation_ArithmaticOperators.mp4
+		//****************************************************************************************************
+
+		//String is a class: default class
+		//class name should start with capital letter
+		
+		String name ="Naveen";
+		String num = "1000";
+		
+		String x = "Hello";
+		String y = "Selenium";
+		
+		int a = 100;
+		int b = 300;
+		
+		System.out.println(a+b);//300
+		
+		
+		
+		
+		
+
+
+	}
+
+}
